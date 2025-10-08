66615b774253f0eb45fb8584cfbbfdb7a428287a
Diya Shah <cclloouudd19@gmail.com>
2025-10-08 15:23:32 +0545

    updated

--- script.js
+++
@@ -1,7 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", () => {
-    const body = document.getElementsById("Hello")
-    body.addEventListener("click", () => {
-        body.style.color = body.style.color === "red" ? "red" : "black";
-    });
-});
