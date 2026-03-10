c047853c575dcbb6304b2892a926c4dc3d2e364a
Diya Shah <cclloouudd19@gmail.com>
2026-03-10 23:58:38 +0545

    js practice

---
+++ ex.js
@@ -0,0 +1,60 @@
+//practice 
+
+// const tree={
+//     color:"red",
+//     hright:"tall",
+//     branch:"2",
+//     age:2,
+
+// };
+// console.log("tree:", tree);
+
+
+
+/*const ball={
+    color: "red",
+    item:"5"
+};
+ball ["color"]= "pink";
+ball ["item"]= "79";*/
+
+     //let num = 5;
+    // let b=6;
+    // console.log(a>=b);
+    // if (b % 2 === 0){
+        // console.log( b , "is even");
+    
+    // }else{
+        // console.log( b , "is odd");
+    // }
+/*let number = prompt("give number");
+let score;
+if ( number %3===0){
+    score="6"
+}
+else{
+     score="7"
+}
+console.log("score" ,score);
+for(let diya=6; diya<=6; diya++){
+    console.log("diya",diya);
+}*/
+//  let i = 2;
+//  do{
+//     console.log(i);
+//     i++;
+//  }while(i<=5);
+
+// let gamenum=12;
+// let num=prompt('guess Num');
+// while(  gamenum!= num){
+//     num=prompt('guess Num');
+// }
+ //console.log("u got a no");
+//  let str="                                    javascript practice     ";
+// str=str.trim();
+//  console.log(str);
+
+// let i= prompt("Enter Full Name");
+// console.log("@".concat(i,i.length));
+
